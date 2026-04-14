7c375ca6167eceda15e0439bf1d524681e3826b8
yag0x1 <yagx01@gmail.com>
2026-04-14 16:35:39 -0300

    main: 3.esp32c3-learning: display-lcd016n002b-control folder added

--- 3.esp32c3-learning/1.display-lcd016n002b-control/components/lcd016n002b/lcd016n002b.c
+++ 3.esp32c3-learning/1.display-lcd016n002b-control/components/lcd016n002b/lcd016n002b.c
@@ -2,4 +2,8 @@
 
 static lcd_pin_config_t *ptr_pin;
 
-void lcd_config_pins(lcd_pin_config_t *pin_config) { ptr_pin = pin_config; };
+void lcd_config_pins(lcd_pin_config_t *pin_config)
+{
+  ptr_pin = pin_config;
+
+};
